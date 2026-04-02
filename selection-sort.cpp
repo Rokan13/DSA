0fcffdee62ea236db57f3b0bcaa6c9046fec94a3
Shah Rokan <121371680+Rokan13@users.noreply.github.com>
2026-04-02 22:58:52 +0600

    Add files via upload

---
+++ selection-sort.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>> n ;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+    for(int i= 0; i < n-1; i++){
+        int smallindex=i;
+        for(int j = i+1; j < n; j++){
+            if(arr[j] < arr[smallindex]){
+                smallindex= j;
+            }
+        }
+        swap(arr[i],arr[smallindex]);
+    }
+    for(int i = 0;i < n; i++){
+        cout<< arr[i]<< " ";
+    }
+    cout<< endl;
+}
