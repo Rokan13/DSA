22a01895701a17ece86a432858ece133e5ce779e
Shah Rokan <121371680+Rokan13@users.noreply.github.com>
2026-04-11 10:45:25 +0600

    Add files via upload

---
+++ Amstrong.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printdigit(int n){
+   int sum = 0;
+
+   while(n!= 0){
+    int digit = n%10;
+    sum +=digit;
+
+    n = n/10;
+   }
+
+}
+
+bool isArmstrong(int n)
+{
+    int copyN=n;
+    int sumofCubes = 0;
+
+    while(n != 0){
+        int dig = n % 10;
+        sumofCubes +=(dig*dig*dig);
+
+        n = n/10;
+    }
+    return sumofCubes == copyN;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    if(isArmstrong(n)){
+        cout << "is an armstrong number\n";
+    }else{
+        cout << "Not armstrong number\n";
+    }
+}
