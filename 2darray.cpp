272901cb32f39e45aefec8a4369a5109d3060cb9
Shah Rokan <121371680+Rokan13@users.noreply.github.com>
2026-04-27 23:22:21 +0600

    Add files via upload

---
+++ 2darray.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int matrix[4][3] = {{1, 2, 3},{4, 5, 6},{7, 8, 9},{10, 11, 12}};
+    int row = 4;
+    int cols = 3;
+
+    cout << matrix[2][1] << endl;
+}
---
+++ 2darrays.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[4][3]={{1, 2, 3},{4, 5, 6},{7, 8, 9},{10, 11, 12}};
+    int rows=4;
+    int cols = 3;
+    for( int i = 0; i <= rows; i++){
+        for(int j = 0; j <= cols; j++){
+            cout << arr[i][j] << " ";
+        }
+    }
+}
